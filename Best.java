d4f3d14dbe2ae48159c21a25920f8b2040817fba
Derek Harrison <hderek18@comcast.net>
2018-01-11 12:23:43 -0700

    Prints the statement specified number of times

---
+++ Best.java
@@ -0,0 +1,12 @@
+class Best {
+	
+	public static void main(String[] args) {
+		int num = Integer.parseInt(args[0]);
+
+		for (int i=0; i<num; i++) {
+			System.out.println("YOU'RE THE BEST!");
+			
+		}
+	}
+
+}
